2cdcb2a58d7fa894ee091fb8e6869a49d5bc2911
fo-ji <fumiya1753@gmail.com>
2022-11-20 15:41:26 +0900

    add suspense

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,23 @@
-import { useEffect } from 'react';
+import { Suspense } from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, Loader } from '@mantine/core';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import axios from 'axios';
+import { ErrorBoundary } from 'react-error-boundary';
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: false,
       refetchOnWindowFocus: false,
+      suspense: true,
+      // useErrorBoundary: true,
     },
   },
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  // MEMO: client - server 間でのやり取りをする上で必要な初期設定
-  axios.defaults.withCredentials = true;
-  // MEMO: 初期レンダリングでCSRFトークンを取得する
-  useEffect(() => {
-    const getCsrfToken = async () => {
-      const { data } = await axios.get(
-        `${process.env.NEXT_PUBLIC_API_URL}/auth/csrf`
-      );
-      axios.defaults.headers.common['csrf-token'] = data.csrfToken;
-    };
-    getCsrfToken();
-  }, []);
-
   return (
     <QueryClientProvider client={queryClient}>
       <MantineProvider
@@ -39,7 +28,11 @@ export default function App({ Component, pageProps }: AppProps) {
           fontFamily: 'Verdana, sans-serif',
         }}
       >
-        <Component {...pageProps} />
+        <ErrorBoundary fallback={<h3>Error...</h3>}>
+          <Suspense fallback={<Loader my="lg" color="cyan" />}>
+            <Component {...pageProps} />
+          </Suspense>
+        </ErrorBoundary>
       </MantineProvider>
       <ReactQueryDevtools />
     </QueryClientProvider>
